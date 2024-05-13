4d0a289b7769db2cf68f46cb6a36e2de1fa615a9
Moulitharann <mouli8376@gmail.com>
2024-05-13 08:37:08 +0530

    updated

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,47 +1,42 @@
-import React from 'react'
-import { Button} from '@mui/material'
-import {auth,provider} from "../../firebase"
-import {signInWithPopup} from 'firebase/auth'
-import {} from "./Login.css"
-import  {useStateValue} from "../contextapi/Stateprovider"
-import  {actionTypes} from "../contextapi/reducer"
-
+import React from 'react';
+import { Button } from '@mui/material';
+import { auth, provider } from '../../firebase';
+import { signInWithPopup } from 'firebase/auth';
+import './Login.css'; // Assuming you have a CSS file for styling
+import { useStateValue } from '../contextapi/Stateprovider';
+import { actionTypes } from '../contextapi/reducer';
 
 const Login = () => {
- 
-  const [state,dispatch]=useStateValue();
- 
-  const signin=()=>{
-    signInWithPopup(auth,provider)  
-    .then((result)=>{
-       dispatch({
-        type:actionTypes.SET_USER,
-        user:result.user,
-       })
-      
-    }).catch((err)=>{
-        alert(err.result)
-        console.log(err.result)
-    })
-  }
+  const [{}, dispatch] = useStateValue();
 
+  const signin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        });
+      })
+      .catch((err) => {
+        alert(err.message);
+        console.error(err.message);
+      });
+  };
 
   return (
-    <div className='login'> 
+    <div className='login'>
       <div className='login__container'>
-           <img
-           src='https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg'
-           alt='logo'
-           />
-           <div className='login__text'>
-            <h1>Sign to Whatsapp</h1>
-            <Button onClick={signin}>
-                Sign with Google
-            </Button>
-           </div>
+        <img
+          src='https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg'
+          alt='logo'
+        />
+        <div className='login__text'>
+          <h1>Sign to Whatsapp</h1>
+          <Button onClick={signin}>Sign with Google</Button>
+        </div>
       </div>
-    </div>  
-  )
-}
+    </div>
+  );
+};
 
-export default Login
+export default Login;
